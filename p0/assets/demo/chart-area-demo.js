a7e25b0614688e4633f9f6d5ad0c147629514732
potasium2 <noahsaber64@aol.com>
2025-09-30 10:57:03 -0400

    working on it
    done (for now):
    rankings - global/friends
    beatmaps - ranked/recent
    
    stuff that needs to be done that shouldnt take too long:
    wiki and its pages
    forums
    tournaments
    
    stuff that will probably take a minute:
    finishing user page
    creating beatmap pages
    creating beatmap submission
    writing out the wiki :D :D :D

--- p0/assets/demo/chart-area-demo.js
+++ p0/assets/demo/chart-area-demo.js
@@ -2,53 +2,85 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+const xValues = []
+const yValues = []
+for(let i = 0; i < 51; i++) {
+  xValues.push(i)
+  yValues.push(1)
+}
+
 // Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+new Chart("playerGraph", {
+type: "line",
+data: {
+    labels: xValues,
     datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  },
-  options: {
+    data: yValues,
+    borderColor: "purple",
+    fill: false,
+    pointRadius: 0.01,
+    cubicInterpolationMode: 'monotone',
+    stepped: false
+    }]
+},
+options: {
     scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
+        x: {
+            ticks: {
+                display: false
+            },
+            grid: {
+                display: false
+            },
+            border: {
+                display: false
+            },
+            reverse: true
         },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
+        y: {
+            ticks: {
+                display: false
+            },
+            grid: {
+                display: false
+            },
+            border: {
+                display: false
+            },
+            reverse: true
         }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
     },
-    legend: {
-      display: false
+    plugins: {
+        legend: {
+            display: false
+        },
+        tooltip: {
+            callbacks: {
+                label: function(context) {
+                    let label = context.dataset.label || '';
+                    if (label) {
+                        label += ': ';
+                    }
+                    label += context.parsed.y;
+                    label = 'Rank: ' + label; 
+                    return label;
+                },
+                title: function(tooltipItems, data) {
+                    const xValue = tooltipItems[0].parsed.x;
+                    if (xValue == 0) {
+                        return 'Now'
+                    }
+                    if (xValue == 1) {
+                        return 'Yesterday'
+                    }
+                    return xValue + ' days ago';
+                }
+            },
+            intersect: false
+        }
+    },
+    animation: {
+        duration: 0
     }
-  }
+}
 });
--- p0/todo.md
+++ p0/todo.md
@@ -1,8 +1,8 @@
 ## General
 ---
-- [ ] Create Placeholder Data for Pages
+- [x] Create Placeholder Data for Pages
 - [x] Redesign Dashboard and Feature New Maps and such
-- [ ] Change Footer Copyright and add Additional Links
+- [x] Change Footer Copyright and add Additional Links
 - [ ] Change the Login Page to not be eye bleeding blue
 
 ## User's And Listings
@@ -10,24 +10,24 @@
 - [ ] Add More User Options
     - [ ] My Profile
     - [ ] Friends
-    - [ ] Settings
+    - [x] Settings
     - [ ] Sign Out
 
-- [ ] Add User Pages
+- [x] Add User Pages
     - [ ] Move Area Graph Here
-    - [ ] Additional User Data
+    - [x] Additional User Data
     - [ ] About Me
     - [ ] Best/Recent Plays
     - [ ] Submitted Maps
 
-- [ ] Change Table Layout
+- [x] Change Table Layout
     - [x] Sort by rank by default
-    - [ ] Remove other sorting methods
-    - [ ] Change data tables styling
+    - [x] Remove other sorting methods
+    - [x] Change data tables styling
 
 ## Maps and Leaderboards
 ---
-- [ ] Add Page to Search for Maps
+- [x] Add Search Bar for Maps
 - [ ] Add Page to Upload Maps
 
 - [ ] Add Map Pages
@@ -42,14 +42,19 @@
 
 ## Other Misc. Stuff
 ---
-- [ ] Potential Forums
+- [ ] Forums
 - [ ] Wiki Page
+    - [ ] Placeholder Wiki Pages
+
 - [ ] Tournament Page
-- [ ] Change the "Logged in as" text
+    - [ ] Badged Tournaments
+    - [ ] Unverified Tournaments
+
+- [x] Change the "Logged in as" text
 
 - [ ] Make Sure Pages are Titled Properly
-    - [ ] User Pages
-    - [ ] Map Pages
+    - [x] User Pages
+    - [x] Map Pages
     - [ ] Everything Else
 
 This isn't a checklist thing but I'm putting this here to remind myself to do something with the cards if possible.
